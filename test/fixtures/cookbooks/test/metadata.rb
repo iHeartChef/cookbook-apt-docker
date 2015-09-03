60db7c98b18cb3a244e63550f40030c22bdedcd3
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-09-03 19:47:06 -0400

    stub out initial vagrant int tests

---
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -0,0 +1,8 @@
+name 'test'
+maintainer 'St. Isidore de Seville'
+maintainer_email 'st.isidore.de.seville@gmail.com'
+license 'MIT'
+description 'Installs/Configures test'
+version '0.1.0'
+
+depends 'apt-docker'
