23263d7fb72cbc21de9fa43c5a490ccd90d9e520
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-09-03 19:46:35 -0400

    add initial attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,3 +13,40 @@ default['apt-docker']['supported-codenames'] = {
   vivid: true,   # Ubuntu 15.04
   wily: true,    # Ubuntu 15.10
 }
+
+uri = 'https://apt.dockerproject.org/repo'
+distribution = "#{node['platform']}-#{node['lsb']['codename']}"
+keyserver = 'hkp://p80.pool.sks-keyservers.net:80'
+key = '58118E89F3A912897C070ADBF76221572C52609D'
+
+default['apt-docker']['repos'].tap do |repo|
+  repo['docker-main'].tap do |value|
+    value['managed'] = true
+    value['uri'] = uri
+    value['distribution'] = distribution
+    value['components'] = ['main']
+    value['deb-src'] = false
+    value['keyserver'] = keyserver
+    value['key'] = key
+  end
+
+  repo['docker-testing'].tap do |value|
+    value['managed'] = false
+    value['uri'] = uri
+    value['distribution'] = distribution
+    value['components'] = ['testing']
+    value['deb-src'] = false
+    value['keyserver'] = keyserver
+    value['key'] = key
+  end
+
+  repo['docker-experimental'].tap do |value|
+    value['managed'] = false
+    value['uri'] = uri
+    value['distribution'] = distribution
+    value['components'] = ['experimental']
+    value['deb-src'] = false
+    value['keyserver'] = keyserver
+    value['key'] = key
+  end
+end
