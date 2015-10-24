1e2f8af4bc17264394152f14f19152d635b88386
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-10-24 12:45:10 -0400

    edit CHANGELOG & bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ apt-docker CHANGELOG
 This file is used to list changes made in each version of the apt-docker
 cookbook.
 
+0.3.0
+-----
+- Sean OMeara (sean@chef.io)
+  - fix Chef 13 deprecation warnings
+
 0.2.0
 -----
 - St. Isidore de Seville (st.isidore.de.seville@gmail.com)
--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'st.isidore.de.seville@gmail.com'
 license 'MIT'
 description 'Installs/Configures apt Docker Vendor-Specific Repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.3.0'
 
 source_url 'https://github.com/st-isidore-de-seville/cookbook-apt-docker'
 issues_url 'https://github.com/st-isidore-de-seville/cookbook-apt-docker/issues'
 
 recipe 'apt-docker::default', 'Installs/Configures apt Docker Vendor-Specific Repository'
 
-depends 'apt', '~> 2.8'
+depends 'apt', '~> 2.9'
 
 supports 'debian'
 supports 'ubuntu'
